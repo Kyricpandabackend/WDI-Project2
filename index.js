11a15ecfaa308a538ea3b7b17f84bc7ab7167dd5
Kyric Livingston <ricingston@gmail.com>
2018-01-10 11:33:17 -0500

    Working Part 1

---
+++ index.js
@@ -0,0 +1,10 @@
+// dependecnceis
+var express = require('express');
+var parser = require('body-parser');
+var hbs = require('express-handlebars');
+var mongoose = require('./db/connection');
+var app = require();
+var bodyParser = require('body-parser')
+
+//mongoose connects
+mongoose.connect('mongodb)
