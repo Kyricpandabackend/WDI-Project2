8bcc2b25da0e603663072462b3aa34566a5a7fe3
Kyric Livingston <ricingston@gmail.com>
2018-01-10 13:50:11 -0500

    Working Part 2

---
+++ db/schema.js
@@ -0,0 +1,3 @@
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/Historia-Bibliotheca');
+var db = mongoose.connection;
--- index.js
+++ index.js
@@ -2,9 +2,18 @@
 var express = require('express');
 var parser = require('body-parser');
 var hbs = require('express-handlebars');
-var mongoose = require('./db/connection');
-var app = require();
+var mongoose = require('mongoose');
+var app = express();
 var bodyParser = require('body-parser')
 
 //mongoose connects
-mongoose.connect('mongodb)
+// mongoose.connect('mongodb://localhost/Historia-Bibliotheca')
+// var db = mongoose.connection
+
+app.get('/', (req, res) => {
+res.send('historyBooks!');
+});
+
+app.listen((4001), () => {
+  console.log('listening on port 4001')
+})
