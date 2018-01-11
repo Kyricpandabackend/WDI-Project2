6ddd0b45ffa8844f056ac79cf7dbef730d2d70af
Kyric Livingston <ricingston@gmail.com>
2018-01-11 11:56:10 -0500

    Finally

--- db/connections.js
+++ db/connections.js
@@ -2,6 +2,9 @@ const mongoose = require('mongoose')
 
 mongoose.Promise = Promise
 
-mongoose.connect('mongodb://localhost/Historia-Bibliotheca', { useMongoClient: true })
-
+if (process.env.NODE_ENV == "production") {
+  mongoose.connect(process.env.MLAB_URL)
+} else {
+  mongoose.connect('mongodb://localhost/Historia-Bibliotheca');
+}
 module.exports = mongoose
